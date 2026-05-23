5a5629697f15c923a32458b891dacb3593f212a3
satyamk7 <satyamk.1107@gmail.com>
2026-05-23 18:35:20 +0530

    Add Todo components and services with initial implementation

---
+++ src/app/components/todo-form/todo-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-form',
+  imports: [],
+  templateUrl: './todo-form.html',
+  styleUrl: './todo-form.css',
+})
+export class TodoForm {
+
+}
