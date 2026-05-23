45befa509e8b2fe89b6b1df7325caaee4816ed12
satyamk7 <satyamk.1107@gmail.com>
2026-05-23 22:00:42 +0530

    Implement Todo service with CRUD operations

--- src/app/services/todo.ts
+++ src/app/services/todo.ts
@@ -1,8 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Todo {
-  
+  private apiUrl = 'http://localhost:8080/api/todos';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+
+  create(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+
+  update(id: number, todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${this.apiUrl}/${id}`, todo);
+  }
+
+  toggleComplete(id: number): Observable<Todo> {
+    return this.http.patch<Todo>(`${this.apiUrl}/${id}/toggle`, {});
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
