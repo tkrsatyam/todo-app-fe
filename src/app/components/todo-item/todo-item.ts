f1fce07be0a833b38397f58da7c3a7b08d1827d4
satyamk7 <satyamk.1107@gmail.com>
2026-05-23 22:35:32 +0530

    Implement TodoItem component with template and event handling

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Todo } from '../../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './todo-item.css',
 })
 export class TodoItem {
+  todo = input.required<Todo>();
+  toggle = output<number>();
+  edit = output<Todo>();
+  delete = output<number>();
 
+  onToggle(): void {
+    this.toggle.emit(this.todo().id!);
+  }
+
+  onEdit(): void {
+    this.edit.emit(this.todo());
+  }
+
+  onDelete(): void {
+    this.delete.emit(this.todo().id!);
+  }
 }
