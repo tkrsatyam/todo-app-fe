0f2d2a369ab3d5af02ffda70ed70bd96b099f6cc
satyamk7 <satyamk.1107@gmail.com>
2026-05-24 15:20:25 +0530

    Implement TodoForm component with form handling and editing capabilities

--- src/app/components/todo-form/todo-form.ts
+++ src/app/components/todo-form/todo-form.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, input, OnChanges, output, SimpleChanges } from '@angular/core';
+import { Todo } from '../../models/todo.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-form.html',
   styleUrl: './todo-form.css',
 })
-export class TodoForm {
+export class TodoForm implements OnChanges {
+  todo = input<Todo | null>();
+  save = output<Todo>();
+  cancel = output<void>();
 
+  formData: Todo = { title: '', description: '', completed: false };
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['todo']) {
+      this.formData = this.todo() ?
+        { ...this.todo()! } : 
+        { title: '', description: '', completed: false };
+    }
+  }
+
+  onSubmit(): void {
+    if (this.formData.title.trim()) {
+      this.save.emit(this.formData);
+    }
+  }
+
+  onCancel(): void {
+    this.cancel.emit();
+  }
+
+  get isEditing(): boolean {
+    return !!this.todo()?.id;
+  }
 }
