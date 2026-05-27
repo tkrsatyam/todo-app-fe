68afeb5c0b8dd291c932004759f1bfe4e91ca96b
satyamk7 <satyamk.1107@gmail.com>
2026-05-27 17:08:32 +0530

    fix: update environment import to use the correct configuration

--- src/app/services/todo.ts
+++ src/app/services/todo.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Todo } from '../models/todo.model';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
